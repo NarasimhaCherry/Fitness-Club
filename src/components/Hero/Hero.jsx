06d3cf26180930060d67acacbd102a8691965270
Narasimha <narasimhacharithartha@gmail.com>
2024-02-19 23:01:12 +0530

    animation commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,15 +5,25 @@ import hero_image from "../../assets/hero_image.png"
 import hero_image_back from "../../assets/hero_image_back.png"
 import Heart from "../../assets/heart.png"
 import Calories from "../../assets/calories.png"
+import NumberCouter from 'number-counter';
+import {motion} from 'framer-motion'
 
 const Hero = () => {
+
+    const transition = {type: 'spring', duration: 3}
+    const mobile = window.innerHeight<=768 ? true: false;
   return (
-    <div className='hero'>
+    <div className='hero' id="home">
+        <div className='blur h-blur'></div>
         <div className='left'>
             <Navbar />
             {/* hover animation */}
             <div className='animation'>
-                <div></div>
+                <motion.div
+                    initial={{left: mobile? "140px": '238px'}}
+                    whileInView={{left: '8px'}}
+                    transition={transition}
+                ></motion.div>
                 <span>It's Your Workout: Your Time</span>
             </div>
 
@@ -36,15 +46,21 @@ const Hero = () => {
             {/* figures */}
             <div className='figures'>
                 <div>
-                    <span>+ 100 </span>
+                    <span>
+                        <NumberCouter end={100} start={50} delay='4' preFix="+" />
+                    </span>
                     <span>expert coaches</span>
                 </div>
                 <div>
-                    <span>+ 900 </span>
+                    <span>
+                    <NumberCouter end={969} start={800} delay='4' preFix="+" />
+                    </span>
                     <span>members joined</span>
                 </div>
                 <div>
-                    <span>+ 40 </span>
+                    <span>
+                    <NumberCouter end={40} start={0} delay='4' preFix="+" />
+                    </span>
                     <span>fitess programs</span>
                 </div>
             </div>
@@ -57,24 +73,37 @@ const Hero = () => {
         </div>
         <div className='right'>
             <button className='btn'>Join Now</button>
-            <div className='heart-rate'>
+
+            <motion.div 
+             initial={{ right: "-1rem" }}
+             whileInView={{ right: "4rem" }}
+             transition={transition}
+             className='heart-rate'>
                 <img src={Heart} alt="Heart" />
                 <span>Heart Rate</span>
                 <span> 110 bpm</span>
-            </div>
+            </motion.div>
 
             {/* hero images */}
             <img src={hero_image} alt="body-builder" className='hero-image' />
-            <img src={hero_image_back} alt="background" className='hero-image-back' />
+            <motion.img 
+             initial={{right: "11rem"}}
+             whileInView={{right: "20rem"}}
+             transition={transition}
+             src={hero_image_back} alt="background" className='hero-image-back' />
 
             {/* calories */}
-            <div className='calories'>
+            <motion.div 
+             initial={{ right: "37rem "}}
+             whileInView={{ right: "28rem" }}
+             transition={transition}
+             className='calories'>
                 <img src={Calories} alt="calories-img" />
                 <div>
                     <span>Calories Burned</span>
                     <span> 200 Kcal</span>
                 </div>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
 import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
+import {Link} from "react-scroll";
 
 const Navbar = () => {
+
+  const mobile = window.innerHeight<=768 ? true: false;
+  const [menuOpened, setMenuOpened] = useState(false)
+
   return (
     <div className='navbar'>
       <img src={Logo} alt="Logo" className='logo' />
-      
+      {menuOpened=== false && mobile===true?(
+        <div style={{backgroundColor: 'var(--appColor)', padding: '0.5rem', borderRadius: '5px',}}
+         onClick={() => setMenuOpened(true)}>
+        <img src={Bars} alt="bars" style={{width: '1.5rem', height: '1.5rem'}} /></div>
+      ): (
       <ul className='menu'>
-        <li>Home</li>
-        <li>Product</li>
-        <li>Review</li>
-        <li>Pricing</li>
-        <li>Contact</li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          activeClass='active'
+          to='home'
+          span={true}
+          smooth={true}
+          >Home</Link></li>
+       <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='programs'
+          span={true}
+          smooth={true}
+          >Programs</Link></li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='reasons'
+          span={true}
+          smooth={true}
+          >Reasons</Link></li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='plans'
+          span={true}
+          smooth={true}
+          >Plans</Link></li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='testimonials'
+          span={true}
+          smooth={true}
+          >Testimonials</Link></li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='join'
+          span={true}
+          smooth={true}
+          >Join</Link></li>
+        <li> 
+          <Link 
+          onClick={()=>setMenuOpened(false)}
+          to='footer'
+          span={true}
+          smooth={true}
+          >Footer</Link></li>
       </ul>
+     )}
     </div>
   )
 }
